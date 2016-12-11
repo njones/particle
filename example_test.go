164e67ef003477f0c35df1b8500031accde3aa9a
Nika Jones <github@nikajon.es>
2016-12-11 20:49:59 +0000

    Adds basic positive test cases and a small example change.

--- example_test.go
+++ example_test.go
@@ -77,7 +77,8 @@ func ExampleNewEncoder() {
 	// Writing to the writer that we got back from the NewEncoder function
 	out.Write([]byte("Content..."))
 
-	fmt.Printf("content: \n%s", w.String())
+	fmt.Printf("content: % x", w.String())
+	// output: content: 2d 2d 2d 0a 6e 61 6d 65 3a 20 41 20 4e 65 77 45 6e 63 6f 64 65 72 20 45 78 61 6d 70 6c 65 0a 2d 2d 2d 0a 0a 43 6f 6e 74 65 6e 74 2e 2e 2e
 }
 
 func ExampleEncodeToString() {
@@ -89,6 +90,7 @@ func ExampleEncodeToString() {
 	// Do the decoding...
 	content := JSONEncoding.EncodeToString(src, &v)
 
-	fmt.Printf("content: \n%s", content)
+	fmt.Printf("content: % x", content)
+	// output: content: 7b 0a 09 22 4e 61 6d 65 22 3a 20 22 41 20 45 6e 63 6f 64 65 54 6f 53 74 72 69 6e 67 20 45 78 61 6d 70 6c 65 22 0a 7d 0a 0a 43 6f 6e 74 65 6e 74 2e 2e 2e
 
 }
---
+++ particle_test.go
@@ -0,0 +1,209 @@
+package particle
+
+import (
+	"bytes"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+var testCaseData = map[string]map[string]string{
+	"YAML": map[string]string{
+		"file": `---
+name: John Doe
+date: 10-10-2016
+title: example YAML
+---
+
+This is an example file.
+`,
+	},
+	"TOML": map[string]string{
+		"file": `+++
+Name = "John Doe"
+Date = "10-10-2016"
+Title = "example TOML"
++++
+
+This is an example file.
+`,
+	},
+	"JSON": map[string]string{
+		"file": `{
+	"Name": "John Doe",
+	"Date": "10-10-2016",
+	"Title": "example JSON"
+}
+
+This is an example file.
+`,
+	},
+}
+
+type testMetaData struct {
+	Name, Date, Title string
+}
+
+var wantInt = 25
+var wantContent = "This is an example file.\n"
+var wantMetaData = testMetaData{Name: "John Doe", Date: "10-10-2016"}
+
+// TODO: Test Large File
+// TODO: Test Bad fontmater block
+// TODO: Test Unicode in the Fontmatter Block
+
+func TestCustomEncoding(t *testing.T) {
+	wantDelimiter := "xoxo"
+	wantOutputDelimiter := true
+
+	var wantMarshalFunc MarshalFunc = func(i interface{}) ([]byte, error) {
+		return []byte("custom: data"), nil
+	}
+
+	var wantUnmarshalFunc UnmarshalFunc = func(b []byte, i interface{}) error {
+		switch oo := i.(type) {
+		case map[string]string:
+			oo["custom"] = "data"
+		}
+		return nil
+	}
+
+	haveEnc := NewEncoding(
+		WithDelimiter(wantDelimiter),
+		WithMarshalFunc(wantMarshalFunc),
+		WithUnmarshalFunc(wantUnmarshalFunc),
+		WithIncludeDelimiter(),
+	)
+
+	if wantDelimiter != haveEnc.delimiter {
+		t.Errorf("want: %+v have: %+v", wantDelimiter, haveEnc.delimiter)
+	}
+
+	if wantOutputDelimiter != haveEnc.outputDelimiter {
+		t.Errorf("want: %+v have: %+v", wantOutputDelimiter, haveEnc.outputDelimiter)
+	}
+
+	// They should have the same pointer (at least)
+	if &wantMarshalFunc == &haveEnc.marshalFunc {
+		t.Errorf("want: %v have: %v", wantMarshalFunc, haveEnc.marshalFunc)
+	}
+
+	if &wantUnmarshalFunc == &haveEnc.unmarshalFunc {
+		t.Errorf("want: %+v have: %+v", wantUnmarshalFunc, haveEnc.unmarshalFunc)
+	}
+}
+
+func TestEncoding(t *testing.T) {
+
+	var runner = []struct {
+		Name     string
+		Encoding *Encoding
+	}{
+		{"YAML", YAMLEncoding},
+		{"TOML", TOMLEncoding},
+		{"JSON", JSONEncoding},
+	}
+
+	for _, r := range runner {
+		t.Log("Testing: " + r.Name)
+
+		wantMetaData.Title = "example " + r.Name
+		wantContentFile := testCaseData[r.Name]["file"]
+
+		haveContent1 := new(bytes.Buffer)
+		out, err := NewEncoder(r.Encoding, haveContent1, wantMetaData)
+		if err != nil {
+			t.Errorf(r.Name+"(NewEncoder): err: %s", err)
+		}
+		out.Write([]byte(wantContent))
+
+		if wantContentFile != haveContent1.String() {
+			t.Errorf(r.Name+"(NewEncoder): \nwant: %+v \nhave: %+v", wantContentFile, haveContent1.String())
+		}
+
+		haveContent2 := r.Encoding.EncodeToString([]byte(wantContent), wantMetaData)
+
+		if wantContentFile != haveContent2 {
+			t.Errorf(r.Name+"(EncodeToString): \nwant: %+v \nhave: %+v", wantContentFile, haveContent2)
+		}
+
+		haveContent3 := make([]byte, len(wantContentFile))
+		r.Encoding.Encode(haveContent3, []byte(wantContent), wantMetaData)
+
+		if wantContentFile != string(haveContent3) {
+			t.Errorf(r.Name+"(Encode): \nwant: %+v \nhave: %+v", wantContentFile, string(haveContent3))
+		}
+	}
+}
+
+func TestDecoding(t *testing.T) {
+
+	var runner = []struct {
+		Name     string
+		Encoding *Encoding
+	}{
+		{"YAML", YAMLEncoding},
+		{"TOML", TOMLEncoding},
+		{"JSON", JSONEncoding},
+	}
+
+	for _, r := range runner {
+		t.Log("Testing: " + r.Name)
+
+		wantMetaData.Title = "example " + r.Name
+		wantContentFile := testCaseData[r.Name]["file"]
+
+		haveMetaData1 := testMetaData{}
+		out, err := NewDecoder(r.Encoding, strings.NewReader(wantContentFile), &haveMetaData1)
+		if err != nil {
+			t.Errorf(r.Name+"(NewDecoder): err: %s", err)
+		}
+
+		haveContent1 := new(bytes.Buffer)
+		haveContent1.ReadFrom(out)
+
+		if wantContent != haveContent1.String() {
+			t.Errorf(r.Name+"(NewDecoder): \nwant: %+v \nhave: %+v", wantContent, haveContent1.String())
+		}
+
+		if !reflect.DeepEqual(wantMetaData, haveMetaData1) {
+			t.Errorf(r.Name+"(DecodeString): \nwant: %+v \nhave: %+v", wantMetaData, haveMetaData1)
+		}
+
+		haveMetaData2 := testMetaData{}
+		haveContent2, err := r.Encoding.DecodeString(wantContentFile, &haveMetaData2)
+		if err != nil {
+			t.Errorf(r.Name+"(DecodeString): err %s", err)
+		}
+
+		if wantContent != string(haveContent2) {
+			t.Errorf(r.Name+"(DecodeString): \nwant: %+v \nhave: %+v", wantContent, string(haveContent2))
+		}
+
+		if !reflect.DeepEqual(wantMetaData, haveMetaData2) {
+			t.Errorf(r.Name+"(DecodeString): \nwant: %+v \nhave: %+v", wantMetaData, haveMetaData2)
+		}
+
+		var (
+			haveMetaData3 = testMetaData{}
+			haveContent3  = make([]byte, wantInt)
+		)
+
+		haveInt3, err := r.Encoding.Decode(haveContent3, []byte(wantContentFile), &haveMetaData3)
+		if err != nil {
+			t.Errorf(r.Name+"(Decode): err %s", err)
+		}
+
+		if wantInt != haveInt3 {
+			t.Errorf(r.Name+"(Decode): \nwant: %+v \nhave: %+v", wantInt, string(haveInt3))
+		}
+
+		if wantContent != string(haveContent3) {
+			t.Errorf(r.Name+"(Decode): \nwant: %+v \nhave: %+v", wantContent, string(haveContent3))
+		}
+
+		if !reflect.DeepEqual(wantMetaData, haveMetaData3) {
+			t.Errorf(r.Name+"(Decode): \nwant: %+v \nhave: %+v", wantMetaData, haveMetaData3)
+		}
+	}
+}
